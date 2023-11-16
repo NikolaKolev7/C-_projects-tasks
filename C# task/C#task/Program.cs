71b4ad1e5d7f0cefc5b8f9ad9cf73b1580ef49f4
Nikola Kolev <nikolakolev7979@gmail.com>
2023-11-16 20:42:40 +0200

    Add files via upload
    I have to enter numbers on the console and it will bring up what time the plane will land, and the numbers I enter ask me the time of departure, the minutes and the duration of the flight.

---
+++ C# task/C#task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_8__page_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете час на излитане (h): ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете минути на излитане (m): ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете продължителност на полета (в минути, m1): ");
+            int m1 = int.Parse(Console.ReadLine());
+
+            // Изчисляваме общото време в минути, включително излитането и продължителността на полета
+            int totalMinutes = h * 60 + m + m1;
+
+            // Изчисляваме часа на кацането (в часове и минути)
+            int landingHour = totalMinutes / 60;
+            int landingMinute = totalMinutes % 60;
+
+            Console.WriteLine($"Самолетът ще кацне в {landingHour} часа и {landingMinute} минути.");
+        }
+    }
+}
