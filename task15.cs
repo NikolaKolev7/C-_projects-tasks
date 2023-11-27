0f8bd0460cf2658b7974bd46b96b2ccc056a44c3
Nikola Kolev <nikolakolev7979@gmail.com>
2023-11-27 22:15:45 +0200

    Add files via upload
    Mutually prime numbers
    Two numbers are mutually prime if they have no common divisor greater than 1.
    Write a program that inputs two integers a and b and checks whether they are mutually prime.
    If yes - outputs Yes, otherwise outputs No and their greatest common divisor.
    Examples Input: 11 12 Output: Yes
    Input: 8 12 Output: No 4

---
+++ task15.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Четене на две цели числа от клавиатурата
+            Console.Write("Въведете първото число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете второто число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            // Проверка дали числата са взаимно прости
+            if (AreCoprime(a, b))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                int gcd = FindGCD(a, b);
+                Console.WriteLine($"No {gcd}");
+            }
+        }
+
+        // Метод за намиране на НОД (най-големия общ делител)
+        static int FindGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        // Метод за проверка дали числата са взаимно прости
+        static bool AreCoprime(int a, int b)
+        {
+            return FindGCD(a, b) == 1;
+        }
+    }
+}
