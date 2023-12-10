ba09a91ca3e7296dd6491c7e3409772c606aa18c
Nikola Kolev <nikolakolev7979@gmail.com>
2023-12-10 13:38:46 +0200

    Add files via upload
    program that checks all possible combinations of a pair of numbers in the interval
    of two given numbers. The output prints the combination whose sum of the numbers is equal to
    a magic number. If there is no combination matching the condition is printed
    message that it was not found.
    Input: the input is read from the console and consists of three lines:
     First line - start of interval - integer in the range [1. . . 999]
     Second line - end of range - integer in range [greater than first number. . . 1000]
     Third line - magic number - integer in the range [1. . . 10000]
    Output: one line should be printed on the console according to the result:
     If a combination is found whose sum of numbers equals the magic number
    o "Combination N:{sequence number} ({first number} + {second number} = {magic number})"
     If no combination matching the condition is found
    o "{the number of all combinations} combinations - neither equals {the magic number}

---
+++ 25 zad/25 zad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Въведете начало на интервала: ");
+        int start = int.Parse(Console.ReadLine());
+
+        Console.Write("Въведете край на интервала: ");
+        int end = int.Parse(Console.ReadLine());
+
+        Console.Write("Въведете магическото число: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        bool foundCombination = false;
+
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                if (i + j == magicNumber)
+                {
+                    Console.WriteLine($"Намерена комбинация: {i} + {j} = {magicNumber}");
+                    foundCombination = true;
+                }
+            }
+        }
+
+        if (!foundCombination)
+        {
+            Console.WriteLine("Не е намерена комбинация.");
+        }
+    }
+}
